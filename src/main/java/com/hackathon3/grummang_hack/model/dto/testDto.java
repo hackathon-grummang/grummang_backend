b7789c301ced88d5e34b4cb8210daa940bbf3d2c
2W <hsp003636@gmail.com>
2024-08-20 11:06:37 +0900

    dir init

---
+++ src/main/java/com/hackathon3/grummang_hack/model/dto/testDto.java
@@ -0,0 +1,5 @@
+package com.hackathon3.grummang_hack.model.dto;
+
+public class testDto {
+    //삭제해도됨
+}
--- src/main/java/com/hackathon3/grummang_hack/model/entity/StoredFile.java
+++ src/main/java/com/hackathon3/grummang_hack/model/entity/StoredFile.java
@@ -1,4 +1,4 @@
-package com.GASB.slack_func.model.entity;
+package com.hackathon3.grummang_hack.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
