14e4f2b598424b72ce52c3c1dc9c56da5d80927e
mandeuk <mandeuk1999@gmail.com>
2024-08-20 16:57:58 +0900

    Feat: findBySpaceId() 추가

--- src/main/java/com/hackathon3/grummang_hack/repository/OrgSaaSRepo.java
+++ src/main/java/com/hackathon3/grummang_hack/repository/OrgSaaSRepo.java
@@ -4,6 +4,10 @@ import com.hackathon3.grummang_hack.model.entity.OrgSaaS;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrgSaaSRepo extends JpaRepository<OrgSaaS, Integer> {
+
+    List<OrgSaaS> findBySpaceId(String temp);
 }
