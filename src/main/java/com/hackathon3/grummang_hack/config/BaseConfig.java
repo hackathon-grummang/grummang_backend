5de8f14fad33ae2a8d598980996f23ab3fa51e5a
2W <hsp003636@gmail.com>
2024-08-20 11:16:16 +0900

    seohyeon - restTemplate설정 추가

--- src/main/java/com/hackathon3/grummang_hack/config/BaseConfig.java
+++ src/main/java/com/hackathon3/grummang_hack/config/BaseConfig.java
@@ -1,7 +1,9 @@
 package com.hackathon3.grummang_hack.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class BaseConfig {
 
 
