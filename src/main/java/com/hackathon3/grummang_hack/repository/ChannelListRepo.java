bb95df3661c1d3a6646f34041b4c5f4052b4ef44
2W <hsp003636@gmail.com>
2024-08-20 14:18:07 +0900

    seohyeon - 레포지토리 조회 함수 추가.

--- src/main/java/com/hackathon3/grummang_hack/repository/ChannelListRepo.java
+++ src/main/java/com/hackathon3/grummang_hack/repository/ChannelListRepo.java
@@ -4,6 +4,9 @@ import com.hackathon3.grummang_hack.model.entity.ChannelList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChannelListRepo extends JpaRepository<ChannelList, Long> {
+    Optional<ChannelList> findByChannelId(String channelId);
 }
