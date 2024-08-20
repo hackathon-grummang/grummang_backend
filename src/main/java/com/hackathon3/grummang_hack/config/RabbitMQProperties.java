4b40b3acea1bec0654af9c3ad21a0ab193c14214
2W <hsp003636@gmail.com>
2024-08-20 15:49:59 +0900

    seohyeon - 세터추가

--- src/main/java/com/hackathon3/grummang_hack/config/RabbitMQProperties.java
+++ src/main/java/com/hackathon3/grummang_hack/config/RabbitMQProperties.java
@@ -1,10 +1,12 @@
 package com.hackathon3.grummang_hack.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "rabbitmq")
 public class RabbitMQProperties {
