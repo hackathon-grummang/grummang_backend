e6513532137636dcf83567ed665b8a28ef15fbdd
2W <hsp003636@gmail.com>
2024-08-20 16:43:12 +0900

    seohyeon - JPA쿼리문 추가

--- src/main/java/com/hackathon3/grummang_hack/repository/ActivitiesRepo.java
+++ src/main/java/com/hackathon3/grummang_hack/repository/ActivitiesRepo.java
@@ -2,6 +2,8 @@ package com.hackathon3.grummang_hack.repository;
 
 import com.hackathon3.grummang_hack.model.entity.Activities;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -9,6 +11,6 @@ import java.util.Optional;
 
 @Repository
 public interface ActivitiesRepo extends JpaRepository<Activities, Long> {
-
-    Optional<Activities> findByEventTsAndEventType(LocalDateTime eventTs, String eventType);
+    @Query("SELECT a FROM Activities a WHERE a.eventTs = :eventTs AND a.eventType = :eventType")
+    Optional<Activities> findByEventTsAndEventType(@Param("eventTs") LocalDateTime eventTs, @Param("eventType") String eventType);
 }
