04769fd89f956feec7a335684978526f70667075
2W <hsp003636@gmail.com>
2024-08-20 15:45:58 +0900

    seohyeon - ModelMapperConfig 추가

---
+++ src/main/java/com/hackathon3/grummang_hack/config/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package com.hackathon3.grummang_hack.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
