28812bec731b8e824f3ba0e518acea4609e665d5
2W <hsp003636@gmail.com>
2024-08-20 10:05:19 +0900

    init

---
+++ src/main/java/com/hackathon3/grummang_hack/GrummangHackApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon3.grummang_hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrummangHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrummangHackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon3/grummang_hack/GrummangHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon3.grummang_hack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrummangHackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
